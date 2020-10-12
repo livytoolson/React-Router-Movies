1be7ab1a0ef6ecc455365894823acd44889a22f9
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-10-12 12:11:07 -0600

    updated index.js file with router wrapper, setMovieList into state with correct data in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,9 @@ export default function App () {
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
           // Study this response with a breakpoint or log statements
+          // console.log(response.data)
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
